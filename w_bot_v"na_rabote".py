d6ac665d920066b8b72c773e39114cdd9dc22f30
dolmat <39152320+dolmat@users.noreply.github.com>
2019-10-04 19:43:26 +0300

    Create w_bot_v"na_rabote".py

---
+++ w_bot_v"na_rabote".py
@@ -0,0 +1,48 @@
+import telebot
+import pyowm
+from telebot.types import Message
+from telebot import types
+
+
+omw = pyowm.OWM('7c0bb0c14f5da7b74d25f7668ef00176', language="ru")
+bot = telebot.TeleBot('447794786:AAHLQdR5Qo-T3XLtgFVUllhhoJsN0n_yM7Q')
+
+@bot.message_handler(commands=["start"])
+def handle_start(message: Message):
+    bot.send_message(message.chat.id, 'Привет! \nНапиши название любого города России, чтобы узнать там погоду :)')
+
+@bot.message_handler(commands=["help"])
+def handle_start(message: Message):
+    bot.send_message(message.chat.id, 'Андрей - лох')
+
+
+@bot.message_handler(content_types=['text'])
+def send_echo(message: Message):
+    try:
+        observation = omw.weather_at_place(message.text)
+        w = observation.get_weather()
+        temp = w.get_temperature('celsius')["temp"]
+
+        answer = "В городе " + message.text + " сейчас " + w.get_detailed_status(
+        ) + "\n"
+        answer += "Температура в районе " + str(round(temp)) + " градусов" "\n\n"
+
+        if temp <= 10:
+            answer += "Очень холодно"
+        elif 10 < temp < 20:
+            answer += "Терпимо, накинь ветровку"
+        else:
+            answer += "Нормально, можно в футболке"
+
+        bot.reply_to(message, answer)
+    except:
+        bot.reply_to(message, 'Напиши название города, а не это...')    
+
+
+@bot.message_handler(content_types=['sticker'])
+def sticker_handler(message: Message):
+    sticker_id = 'CAADAgAD9QEAArJh9gP2aVyuvJNBwBYE'
+    bot.send_sticker(message.chat.id, sticker_id)
+
+
+bot.polling(none_stop=True, interval=0)
